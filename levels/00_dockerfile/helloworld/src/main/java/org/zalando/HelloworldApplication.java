5c4430b8a89e2250d0c4a5cb5aa69f2975474522
Henning Jacobs <henning@jacobs1.de>
2016-07-25 15:42:31 +0200

    fix OAuth config

--- levels/00_dockerfile/helloworld/src/main/java/org/zalando/HelloworldApplication.java
+++ levels/00_dockerfile/helloworld/src/main/java/org/zalando/HelloworldApplication.java
@@ -9,17 +9,7 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 import org.zalando.stups.oauth2.spring.server.TokenInfoResourceServerTokenServices;
 
 @SpringBootApplication
-@EnableResourceServer
 public class HelloworldApplication {
-
-    @Value("${tokeninfo.url}")
-    private String tokeninfoUrl;
-
-    @Bean
-    public ResourceServerTokenServices customResourceTokenServices() {
-
-        return new TokenInfoResourceServerTokenServices(tokeninfoUrl);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
