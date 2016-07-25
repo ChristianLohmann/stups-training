9a824ba72be5bb2ab84d6d897021d6b5dffae7bf
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-07-25 17:24:03 +0200

    fix imports

--- levels/00_dockerfile/helloworld/src/main/java/org/zalando/HelloworldApplication.java
+++ levels/00_dockerfile/helloworld/src/main/java/org/zalando/HelloworldApplication.java
@@ -1,12 +1,7 @@
 package org.zalando;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.zalando.stups.oauth2.spring.server.TokenInfoResourceServerTokenServices;
 
 @SpringBootApplication
 public class HelloworldApplication {
